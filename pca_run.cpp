cb47afb3a94e788e928f57e987b649bb6a76b1b3
侯奇 <houqi1993@gmail.com>
2017-03-04 16:52:45 +0800

    Update run_pca.cpp
    fix uint64 to uint64_t

--- pca_run.cpp
+++ pca_run.cpp
@@ -37,7 +37,7 @@ long long GetTimeMs64() {
  /* Linux */
  struct timeval tv;
  gettimeofday(&tv, NULL);
- uint64 ret = tv.tv_usec;
+ uint64_t ret = tv.tv_usec;
  /* Convert from micro seconds (10^-6) to milliseconds (10^-3) */
  ret /= 1000;
  /* Adds the seconds (10^0) after converting them to milliseconds (10^-3) */
